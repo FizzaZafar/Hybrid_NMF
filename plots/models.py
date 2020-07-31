061e2caecf609f98ab1701f0e65ad6afe7e3a0bc
Fizza Zafar <fizza.zafar97@gmail.com>
2020-07-31 13:27:11 +0200

    tiny fix

--- plots/models.py
+++ plots/models.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import matplotlib as mp
 import ast
 
-df_baseline = pd.read_csv("../results/models.csv")
+df_models = pd.read_csv("../results/models.csv")
 fig,ax1 = plt.subplots(1,1, figsize=(10,6))
-g = sns.barplot(x="Algorithm",y="RMSE", hue="Type", data=df_baseline, ax=ax1)
+g = sns.barplot(x="Algorithm",y="RMSE", hue="Type", data=df_models, ax=ax1)
 for p in g.patches:
 	g.annotate(format(p.get_height(), '.5f'), (p.get_x() + p.get_width() / 2., p.get_height()), ha = 'center', va = 'center', xytext = (0, 10), textcoords = 'offset points', fontsize=6)
 #sns.barplot(x="Algorithm",y="Test RMSE", data=df_baseline, ax=ax2)
